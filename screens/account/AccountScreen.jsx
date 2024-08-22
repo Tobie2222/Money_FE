92a9313f1bb26c6ae0e4088a853fbaf31bd93081
huuhoang150424 <nguyenhoanghuu15042004@gmail.com>
2024-08-22 21:27:04 +0700

    commit

--- screens/account/AccountScreen.jsx
+++ screens/account/AccountScreen.jsx
@@ -5,8 +5,19 @@ import {  View ,StyleSheet,Image,Text, StatusBar, ScrollView,TouchableOpacity,Mo
 export default function AccountScreen() {
     const {t}=useTranslation()
     return (
-        <View className="flex-1">
-            
+        <View className="flex-1 ">
+            <StatusBar
+                barStyle="light"
+            />
+            <View className="">
+                <View className="bg-primaryColor py-[70px]">
+                    <Text className=" text-[20px] font-[700] text-white text-center">Ví của bạn </Text>
+                </View>
+            <View className="bg-green-500 w-full h-[30px] rounded-t-[8px]">
+
+            </View>
+
+            </View>
         </View>
     )
 }
