e2998769edd4995791b7f9fead8b27d21c4b08d3
ThaiDoanh04 <doanhthai604204@gmail.com>
2024-09-24 15:53:27 +0700

    storage

---
+++ utils/storage.js
@@ -0,0 +1,36 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+
+export const saveData = async (key,data) => {
+    try {
+        await AsyncStorage.setItem(key,data)
+    } catch (err) {
+        console.log(err)
+    }
+}
+export const readData = async (key) => {
+    try {
+        const data = await AsyncStorage.getItem(key)
+        return data ? JSON.parse(data) : null
+    } catch (err) {
+        console.log(err)
+    }
+}
+export const removeData=async(key)=>{
+    try {
+        await AsyncStorage.removeItem(key)
+        console.log(`${key} đã bị xóa khỏi AsyncStorage`)
+        return true
+    } catch(err) {
+        console.log(err)
+    }
+}
+export const removeAllData = async () => {
+    try {
+        await AsyncStorage.clear();
+        return true;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+};
