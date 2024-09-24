2b276eeb5ce8d27c8bbf838bc833e47613acef3d
ThaiDoanh04 <doanhthai604204@gmail.com>
2024-09-24 15:55:00 +0700

    accountSlice

---
+++ redux/accountSlice/index.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const accountSlice = createSlice({
+    name: 'account',
+    initialState: {
+        refresh: true,
+        accounts: [],
+        tranThisMonth: {
+            tranExpense:{
+                type:"expense",
+                amount: 0
+            },
+            tranIncome:{
+                type:"expense",
+                amount: 0
+            },
+        }
+    },
+    reducers: {
+        toggleRefresh(state) {
+            state.refresh = !state.refresh 
+        },
+        getAccounts(state,action) {
+            state.accounts=action.payload.accounts
+        },
+        getTranThisMount(state,action) {
+            state.tranThisMonth=action.payload.tranThisMonth
+        }
+    }
+})
+
+export const { toggleRefresh,getAccounts,getTranThisMount} = accountSlice.actions
+
+//selector
+export const selectRefresh = (state) => state.account.refresh
+export const selectAccounts = (state) => state.account.accounts
+export const selectTranThisMonth = (state) => state.account.tranThisMonth
+
+export default accountSlice.reducer
