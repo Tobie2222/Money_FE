d7e89d3183a1f08957d265fc8ff5ea585ed0adea
ThaiDoanh04 <doanhthai604204@gmail.com>
2024-09-24 15:47:14 +0700

    HomeStack  finish

---
+++ navigation/HomeStackScreen.jsx
@@ -0,0 +1,14 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import HomeScreen from '../screens/home/HomeScreen'
+import NotificationScreen from '../screens/home/NotificationScreen';
+
+const HomeStack = createStackNavigator()
+
+export default function HomeStackScreen() {
+    return (
+        <HomeStack.Navigator screenOptions={{ headerShown: false }}>
+            <HomeStack.Screen name="homeScreen" component={HomeScreen} />
+            <HomeStack.Screen name="notificationScreen" component={NotificationScreen} />
+        </HomeStack.Navigator>
+    );
+}
